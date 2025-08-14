3427fc28546ce59ec0099ef2597fea572b344ed0
HANSOL LEE <hansol2886@gmail.com>
2025-08-14 13:01:34 +0900

    DB연결을 위한 DBUtil.java 생성
    이한솔_250814

---
+++ Anabada/src/main/java/com/yedam/common/DBUtil.java
@@ -0,0 +1,28 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+// MyBatis 환경을 읽어들이고 SqlSessionFactory를 생성해주는 기능
+public class DBUtil {
+
+	public static SqlSessionFactory getInstance() {
+		String resource = "config/mybatis-config.xml";
+		
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		return sqlSessionFactory;
+	}
+}
+
+
+
