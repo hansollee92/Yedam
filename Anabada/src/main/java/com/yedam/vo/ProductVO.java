2ed15af9c95832166013cd09437db6f2362a1812
O deng <oms9623@naver.com>
2025-08-14 17:30:58 +0900

    ProductVO.java 추가
    오라클과 매칭해서 코드 작성하고 저장
    
    오민수_250814

--- Anabada/src/main/java/com/yedam/vo/ProductVO.java
+++ Anabada/src/main/java/com/yedam/vo/ProductVO.java
@@ -1,5 +1,22 @@
 package com.yedam.vo;
 
-public class ProductVO {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class ProductVO {
+	private int prdNo;          // 상품번호 (PK)
+    private String prdName;     // 상품명
+    private long price;         // 상품가격
+    private Date prdDate;       // 등록일자
+    private String category;    // 카테고리
+    private String prdStatus;   // 상품상태 (새것, 사용감있음 등등..)
+    private String prdDesc;     // 상품설명
+    private String prdTag;      // 태그
+    private String tradeType;   // 거래방식
+    private String field;       // 지역
+    private String saleStatus;  // 판매상태 (판매완, 판매중, 예약중 등등..)
+    private String prdImg;      // 상품이미지명
+    private int memberNo;       // 유저번호 (FK)
 }
