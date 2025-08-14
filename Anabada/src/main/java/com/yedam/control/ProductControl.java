1ad22a2183f2f6fb698c9316dcf98b9490ef4074
bong <p1-02@naver.com>
2025-08-14 17:26:51 +0900

    상품 Control 추가
    박봉근_20250814

--- Anabada/src/main/java/com/yedam/control/ProductControl.java
+++ Anabada/src/main/java/com/yedam/control/ProductControl.java
@@ -7,12 +7,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedam.common.Control;
+import com.yedam.mapper.ProductMapper;
+import com.yedam.service.ProductService;
+import com.yedam.service.ProductServiceImpl;
+import com.yedam.vo.ProductVO;
 
 public class ProductControl implements Control {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String memberNo = req.getParameter("memberNo");
+
+		ProductService svc = new ProductServiceImpl();
+		ProductVO product = svc.searchWish(Integer.parseInt(memberNo));
+
+		req.setAttribute("memberNo", memberNo);
+		req.getRequestDispatcher("mypage/wish.tiles").forward(req, resp);
 
 	}
 
