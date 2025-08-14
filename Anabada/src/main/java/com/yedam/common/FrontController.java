72042977625369c7885c5d319f2fbd81d55aac26
O deng <oms9623@naver.com>
2025-08-14 17:22:37 +0900

    FrontController 수정

--- Anabada/src/main/java/com/yedam/common/FrontController.java
+++ Anabada/src/main/java/com/yedam/common/FrontController.java
@@ -1,2 +1,57 @@
 package com.yedam.common;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.control.JoinControl;
+import com.yedam.control.JoinFormControl;
+import com.yedam.control.LoginControl;
+import com.yedam.control.LoginFormControl;
+import com.yedam.control.LogoutControl;
+import com.yedam.control.PayControl;
+import com.yedam.control.PayFormControl;
+
+public class FrontController extends HttpServlet {// HttpServlet를 상속받음
+		
+	Map<String, Control> map;
+	
+	// 생성자
+	public FrontController() {
+		map = new HashMap<String, Control>();
+	}
+	
+	// 메소드
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		// Start 오민수
+		map.put("/joinForm.do", new JoinFormControl());		// 회원가입 화면
+		map.put("/join.do", new JoinControl());				// 회원가입 기능
+		map.put("/loginForm.do", new LoginFormControl());	// 로그인 화면
+		map.put("/login.do", new LoginControl());			// 로그인 기능
+		map.put("/logout.do", new LogoutControl());			// 로그아웃 기능
+		map.put("/payForm.do", new PayFormControl());		// 결제 화면
+		map.put("/pay.do", new PayControl());				// 결제 기능
+		// End 오민수
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// url , uri
+		// http://localhost/Anabada/join.do => url
+		// /Anabada/join.do                      => uri
+		String uri = req.getRequestURI();
+		String context = req.getContextPath();// Anabada
+		String page = uri.substring(context.length());
+		
+		Control control = map.get(page);
+		control.execute(req, resp);
+	}
+}
