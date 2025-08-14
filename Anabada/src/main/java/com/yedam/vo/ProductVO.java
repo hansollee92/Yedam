5f35d63ace58acca73bd6ba3202140abb149c26f
heochangbeom <heoc4869@gmail.com>
2025-08-14 17:31:24 +0900

    상품 테이블의 칼럼 필드 선언
    제곧내
    
    허창범_20250814

--- Anabada/src/main/java/com/yedam/vo/ProductVO.java
+++ Anabada/src/main/java/com/yedam/vo/ProductVO.java
@@ -1,5 +1,17 @@
 package com.yedam.vo;
 
 public class ProductVO {
-
+	private int prdNo;
+	private String prdName;
+	private int price;
+	private int prdDate;
+	private String category;
+	private String prdstatus;
+	private String prddesc;
+	private String prdtag;
+	private String tradeType;
+	private String field;
+	private String saleStatus;
+	private String prdImg;
+	private int    memberNo;
 }
