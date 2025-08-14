a602130469dde30a23b2c29f0023bbd5c03d10eb
bong <p1-02@naver.com>
2025-08-14 18:10:23 +0900

    삭제 및 변경
    박봉근_20250814

--- Anabada/src/main/java/com/yedam/vo/memberVO.java
+++ Anabada/src/main/java/com/yedam/vo/memberVO.java
@@ -6,16 +6,14 @@ import java.util.List;
 
 @Data
 public class MemberVO {
-    private int memberNo;
-    private String memberId;
-    private String memberPw;
-    private String memberName;
-    private String memberBirth;
-    private int memberPhone;
-    private String memberImg;
-    private String memberScore;
-
-    private List<ProductVO> wish;
-
+	private List<ProductVO> wish;
+	private int memberNo;          //유저번호(회원번호)
+	private String memberId;       //아이디
+	private String memberPw;       //비밀번호
+	private String memberName;     //이름
+	private String memberBirth;    //생년월일
+	private int memberPhone;       //전화번호
+	private String memberImg;      //멤버이미지(default null 들어갈 수 있게 할 수 있나?)
+	private int memberScore;       //평점
 }
 
