d643af1fd3c9bd6c95310fb2fe4a8ac46ff4e1bd
O <oms9623@naver.com>
2025-08-16 01:30:52 +0900

    생년월일, 휴대폰번호 변경
    생년월일 String -> Date로 변경
    휴대폰번호 int -> String 으로 변경
    전에 그렇게 하기로 했었음
    
    오민수_250816

--- Anabada/src/main/java/com/yedam/vo/MemberVO.java
+++ Anabada/src/main/java/com/yedam/vo/MemberVO.java
@@ -1,8 +1,9 @@
 package com.yedam.vo;
 
-import lombok.Data;
-
+import java.util.Date;
 import java.util.List;
+
+import lombok.Data;
 
 @Data
 public class MemberVO {
@@ -11,8 +12,8 @@ public class MemberVO {
 	private String memberId;       //아이디
 	private String memberPw;       //비밀번호
 	private String memberName;     //이름
-	private String memberBirth;    //생년월일
-	private int memberPhone;       //전화번호
+	private Date memberBirth;      //생년월일
+	private String memberPhone;    //전화번호
 	private String memberImg;      //멤버이미지(default null 들어갈 수 있게 할 수 있나?)
 	private int memberScore;       //평점
 }
