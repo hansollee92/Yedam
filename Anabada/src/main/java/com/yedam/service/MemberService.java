88abc828f466a6c6ff220f8b2149acd12c943e13
hansoring <hansol2886@gmail.com>
2025-08-17 10:45:38 +0900

    로그인
    이한솔_250817

--- Anabada/src/main/java/com/yedam/service/MemberService.java
+++ Anabada/src/main/java/com/yedam/service/MemberService.java
@@ -4,8 +4,9 @@ import com.yedam.vo.MemberVO;
 
 public interface MemberService {
 	
-    public MemberVO searchMember(int memberNo);      //로그인
+    public MemberVO searchMember(String memberId, String memberPw);      //로그인처리
     boolean registerMember(MemberVO vo);             //회원가입 
     boolean isDuplicateId(String memberId);          //아이디 중복여부(있으면 true)
+	public MemberVO searchMember(int int1);
     
 }
