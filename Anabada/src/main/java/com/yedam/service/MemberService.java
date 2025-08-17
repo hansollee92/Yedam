492048c935132d848860b8e7b75bbab26c10f81a
hansoring <hansol2886@gmail.com>
2025-08-17 10:00:09 +0900

    memberMapper, Service 정리
    이한솔_250817

--- Anabada/src/main/java/com/yedam/service/MemberService.java
+++ Anabada/src/main/java/com/yedam/service/MemberService.java
@@ -3,11 +3,9 @@ package com.yedam.service;
 import com.yedam.vo.MemberVO;
 
 public interface MemberService {
-    public MemberVO searchMember(int memberNo);
+	
+    public MemberVO searchMember(int memberNo);      //로그인
+    boolean registerMember(MemberVO vo);             //회원가입 
+    boolean isDuplicateId(String memberId);          //아이디 중복여부(있으면 true)
     
-    // 회원가입
-    boolean registerMember(MemberVO vo);
-
-    // 아이디 중복 여부 (있으면 true)
-    boolean isDuplicateId(String memberId);
 }
