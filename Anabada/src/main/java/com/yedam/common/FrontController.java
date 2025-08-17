d31a5cf5c3568161b94ef093282aed13d780c547
bong <p1-02@naver.com>
2025-08-18 02:10:30 +0900

    마이페이지 분리
    내용: fetch를 이용하기 위해 분리
    
    박봉근_20250817

--- Anabada/src/main/java/com/yedam/common/FrontController.java
+++ Anabada/src/main/java/com/yedam/common/FrontController.java
@@ -13,46 +13,48 @@ import javax.servlet.http.HttpServletResponse;
 import com.yedam.control.*;
 
 public class FrontController extends HttpServlet {// HttpServlet를 상속받음
-		
-		Map<String, Control> map;
 
-		// 생성자
-		public FrontController() {
-			map = new HashMap<String, Control>();
-		}
+    Map<String, Control> map;
 
-		// 메소드
-		@Override
-		public void init(ServletConfig config) throws ServletException {
+    // 생성자
+    public FrontController() {
+        map = new HashMap<String, Control>();
+    }
 
-			// Start 오민수
-			map.put("/joinForm.do", new JoinFormControl());        // 회원가입 화면
-			map.put("/join.do", new JoinControl());                // 회원가입 기능
-			map.put("/joinSuccess.do", new JoinSuccessControl());  // 회원가입 성공 시 보이는 화면
-			map.put("/checkId.do", new CheckIdControl());		       // 아이디 중복체크
-			map.put("/loginForm.do", new LoginFormControl());      // 로그인 화면
-			map.put("/login.do", new LoginControl());              // 로그인 기능
-			map.put("/logout.do", new LogoutControl());            // 로그아웃 기능
-			map.put("/payForm.do", new PayFormControl());          // 결제 화면
-			map.put("/pay.do", new PayControl());                  // 결제 기능
-			// End 오민수
-      
-			map.put("/mypage.do", new MypageControl());
-			map.put("/product.do", new ProductControl()); //상품 상세
-			map.put("/productList.do", new ProductListControl()); //상품리스트
-      map.put("/main.do", new MainControl());	//메인페이지
-		}
+    // 메소드
+    @Override
+    public void init(ServletConfig config) throws ServletException {
 
-		@Override
-		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			// url , uri
-			// http://localhost/Anabada/join.do => url
-			// /Anabada/join.do                      => uri
-			String uri = req.getRequestURI();
-			String context = req.getContextPath();// Anabada
-			String page = uri.substring(context.length());
+        // Start 오민수
+        map.put("/joinForm.do", new JoinFormControl());        // 회원가입 화면
+        map.put("/join.do", new JoinControl());                // 회원가입 기능
+        map.put("/joinSuccess.do", new JoinSuccessControl());  // 회원가입 성공 시 보이는 화면
+        map.put("/checkId.do", new CheckIdControl());		       // 아이디 중복체크
+        map.put("/loginForm.do", new LoginFormControl());      // 로그인 화면
+        map.put("/login.do", new LoginControl());              // 로그인 기능
+        map.put("/logout.do", new LogoutControl());            // 로그아웃 기능
+        map.put("/payForm.do", new PayFormControl());          // 결제 화면
+        map.put("/pay.do", new PayControl());                  // 결제 기능
+        // End 오민수
 
-			Control control = map.get(page);
-			control.execute(req, resp);
-		}
-	}
+        map.put("/product.do", new ProductControl()); //상품 상세
+        map.put("/productList.do", new ProductListControl()); //상품리스트
+        map.put("/main.do", new MainControl());	//메인페이지
+
+        map.put("/mypage.do", new MypageControl()); // 마이페이지
+        map.put("/wishList.do", new WishListControl()); // 찜목록
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // url , uri
+        // http://localhost/Anabada/join.do => url
+        // /Anabada/join.do                      => uri
+        String uri = req.getRequestURI();
+        String context = req.getContextPath();// Anabada
+        String page = uri.substring(context.length());
+
+        Control control = map.get(page);
+        control.execute(req, resp);
+    }
+}
