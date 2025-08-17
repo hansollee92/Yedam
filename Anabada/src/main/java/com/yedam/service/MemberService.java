b79b5ca48ef87b9a097aa4de501d5a52794a6181
bong <p1-02@naver.com>
2025-08-17 12:40:47 +0900

    주석 옆으로 변경
    박봉근_20250817

--- Anabada/src/main/java/com/yedam/service/MemberService.java
+++ Anabada/src/main/java/com/yedam/service/MemberService.java
@@ -3,11 +3,7 @@ package com.yedam.service;
 import com.yedam.vo.MemberVO;
 
 public interface MemberService {
-    public MemberVO searchMember(int memberNo);
-    
-    // 회원가입
-    boolean registerMember(MemberVO vo);
-
-    // 아이디 중복 여부 (있으면 true)
-    boolean isDuplicateId(String memberId);
+    MemberVO searchMember(int memberNo); // 마이페이지 회원 정보
+    boolean registerMember(MemberVO vo); // 회원가입
+    boolean isDuplicateId(String memberId); // 아이디 중복 여부 (있으면 true)
 }
