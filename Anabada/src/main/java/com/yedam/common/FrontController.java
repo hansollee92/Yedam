e734690f47bb4a7bf4d5d7bb1dead9113724ff1e
HANSOL LEE <hansol2886@gmail.com>
2025-08-18 10:16:32 +0900

    오류수정 이한솔_250817

--- Anabada/src/main/java/com/yedam/common/FrontController.java
+++ Anabada/src/main/java/com/yedam/common/FrontController.java
@@ -42,15 +42,15 @@ import com.yedam.control.WishListControl;
 		public void init(ServletConfig config) throws ServletException {
 
 			// Start 오민수
-        map.put("/joinForm.do", new JoinFormControl());        // 회원가입 화면
-        map.put("/join.do", new JoinControl());                // 회원가입 기능
-        map.put("/joinSuccess.do", new JoinSuccessControl());  // 회원가입 성공 시 보이는 화면
-        map.put("/checkId.do", new CheckIdControl());		       // 아이디 중복체크
-        map.put("/loginForm.do", new LoginFormControl());      // 로그인 화면
-        map.put("/login.do", new LoginControl());              // 로그인 기능
-        map.put("/logout.do", new LogoutControl());            // 로그아웃 기능
-        map.put("/payForm.do", new PayFormControl());          // 결제 화면
-        map.put("/pay.do", new PayControl());                  // 결제 기능
+			map.put("/joinForm.do", new JoinFormControl());        // 회원가입 화면
+			map.put("/join.do", new JoinControl());                // 회원가입 기능
+			map.put("/joinSuccess.do", new JoinSuccessControl());  // 회원가입 성공 시 보이는 화면
+			map.put("/checkId.do", new CheckIdControl());		   // 아이디 중복체크
+			map.put("/loginForm.do", new LoginFormControl());      // 로그인 화면
+			map.put("/login.do", new LoginControl());              // 로그인 기능
+			map.put("/logout.do", new LogoutControl());            // 로그아웃 기능
+			map.put("/payForm.do", new PayFormControl());          // 결제 화면
+			map.put("/pay.do", new PayControl());                  // 결제 기능
 			// End 오민수
 			
 			map.put("/main.do", new MainControl());	//메인페이지
@@ -59,13 +59,13 @@ import com.yedam.control.WishListControl;
 			map.put("/productList.do", new ProductListControl());                //상품리스트
 			map.put("/product.do", new ProductControl());                        //상품 상세 + 조회수
 			map.put("/productModifyForm.do", new ProductModifyFormControl());    //상품 수정 화면 
-			map.put("/prodcutModify.do", new ProductModifyControl());            //상품 수정 처리(post)
+			map.put("/productModify.do", new ProductModifyControl());            //상품 수정 처리(post)
 			map.put("/productRemove.do", new ProdcutRemoveControl());            //상품 삭제
 			
 			// 회원
 			
 			// 마이페이지
-			  	map.put("/wishList.do", new WishListControl()); // 찜목록
+			map.put("/wishList.do", new WishListControl()); // 찜목록
 			
 			
 		}
@@ -74,7 +74,7 @@ import com.yedam.control.WishListControl;
 		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			// url , uri
 			// http://localhost/Anabada/join.do => url
-			// /Anabada/join.do                      => uri
+			// /Anabada/join.do                 => uri
 			String uri = req.getRequestURI();
 			String context = req.getContextPath();// Anabada
 			String page = uri.substring(context.length());
