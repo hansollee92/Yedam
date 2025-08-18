a4dfd0917119458c2fa94e7f350524a8ddb5f828
bong <p1-02@naver.com>
2025-08-18 10:24:51 +0900

    오류 수정
    박봉근_20250818

--- Anabada/src/main/java/com/yedam/mapper/ProductMapper.java
+++ Anabada/src/main/java/com/yedam/mapper/ProductMapper.java
@@ -11,16 +11,16 @@ public interface ProductMapper {
 
     public List<ProductVO> selectProductList();     //상품 목록
     public ProductVO selectProduct(int prdNo);      //상품 단건조회
-    public int updateCntProduct(int prdNo);         //상품 조회수증가 
+    public int updateCntProduct(int prdNo);         //상품 조회수증가
     public int updateProduct(ProductVO product);    //상품 수정
-    
-    public List<ProductVO> productList(ProductVO dao);
-	  public List<ProductVO> selectWish(int memberNo); // 내가 찜한 상품 조회
 
-	  public int updateProductImage(Map<String, Object> param); // 상품 이미지 경로 업데이트
-	  public String getProductImage(int prdNo); // 상품 이미지 경로 조회
-	  public int clearProductImage(int prdNo); // 상품 이미지 경로 삭제
-  
+    public List<ProductVO> productList(ProductVO dao);
+    public List<ProductVO> selectWish(int memberNo); // 내가 찜한 상품 조회
+
+    public int updateProductImage(Map<String, Object> param); // 상품 이미지 경로 업데이트
+    public String getProductImage(int prdNo); // 상품 이미지 경로 조회
+    public int clearProductImage(int prdNo); // 상품 이미지 경로 삭제
+
     public int insertProduct(ProductVO vo); // 상품등록
     
 
