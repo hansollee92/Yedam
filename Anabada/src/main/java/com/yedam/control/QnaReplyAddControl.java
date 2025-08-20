cc1244b2092ff99b04d7db375baedb2b1eafe6da
HANSOL LEE <hansol2886@gmail.com>
2025-08-20 11:16:05 +0900

    이미지 추가

--- Anabada/src/main/java/com/yedam/control/QnaReplyAddControl.java
+++ Anabada/src/main/java/com/yedam/control/QnaReplyAddControl.java
@@ -1,18 +1,21 @@
 package com.yedam.control;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.yedam.common.Control;
 import com.yedam.service.QnaReplyService;
 import com.yedam.service.QnaReplyServiceImpl;
+import com.yedam.vo.MemberVO;
 import com.yedam.vo.QnaReplyVO;
 
 public class QnaReplyAddControl implements Control {
@@ -26,25 +29,26 @@ public class QnaReplyAddControl implements Control {
 		// param : qnaReplyContent, memberNo, qnaNo		
 		String qnaNo = req.getParameter("qnaNo");
 		String qnaReplyContent = req.getParameter("qnaReplyContent");
-		String memberNo = req.getParameter("memberNo");
 		
-//		//권한확인
-//		HttpSession session = req.getSession();
-//		MemberVO logMember = (MemberVO) session.getAttribute("logMember");
-//		
-//		if(logMember == null) {	
-//		    resp.setContentType("text/html; charset=UTF-8");
-//		    PrintWriter out = resp.getWriter();
-//		    out.println("<script>");
-//		    out.println("alert('로그인 후 이용해주세요.');");
-//		    out.println("history.back();");  // 이전 페이지로
-//		    out.println("</script>");
-//		    out.close();
-//		    return;
-//		}
+		//권한확인
+		HttpSession session = req.getSession();
+		MemberVO logMember = (MemberVO) session.getAttribute("logMember");
+		
+		if(logMember == null) {	
+		    resp.setContentType("text/html; charset=UTF-8");
+		    PrintWriter out = resp.getWriter();
+		    out.println("<script>");
+		    out.println("alert('로그인 후 이용해주세요.');");
+		    out.println("history.back();");  // 이전 페이지로
+		    out.println("</script>");
+		    out.close();
+		    return;
+		}
+		
+		int memberNo = logMember.getMemberNo();
 		
 		QnaReplyVO qnaReply = new QnaReplyVO();
-		qnaReply.setMemberNo(Integer.parseInt(memberNo));
+		qnaReply.setMemberNo(memberNo);
 		qnaReply.setQnaNo(Integer.parseInt(qnaNo));
 		qnaReply.setQnaReplyContent(qnaReplyContent);	
 		
