bfd6877f14c1fc51b19255c51aaacfbfafd3ae3a
HANSOL LEE <hansol2886@gmail.com>
2025-08-20 09:16:22 +0900

    CommuMapper 수정, table 데이터타입 길이 늘리기
    이한솔_250820

--- Anabada/src/main/java/com/yedam/mapper/CommuMapper.java
+++ Anabada/src/main/java/com/yedam/mapper/CommuMapper.java
@@ -9,11 +9,11 @@ import com.yedam.vo.CommuVO;
 
 public interface CommuMapper {
 
-	public List<CommuVO> selcetCommu(int commuNo);  // 전체 조회  
-	public CommuVO selectCommuOne(int commuNo); // 단건 조회
-	public int insertCommu(CommuVO vo); // 등록
-	public int update(CommuVO vo);  // 수정
-	public int delete(@Param("commuNo") int commuNo); // 삭제
+	public List<CommuVO> selectCommu(int commuNo);             // 전체 조회  
+	public CommuVO selectCommuOne(int commuNo);                // 단건 조회
+	public int insertCommu(CommuVO vo);                        // 등록
+	public int updateCommu(CommuVO vo);                        // 수정
+	public int deleteCommu(@Param("commuNo") int commuNo);     // 삭제
 	
 }
 
