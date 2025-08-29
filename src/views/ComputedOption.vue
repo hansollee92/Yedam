7e7bb8009412212c42aed71488067d867b797bd5
HANSOL LEE <hansol2886@gmail.com>
2025-08-29 12:59:11 +0900

    ✨ watch 속성 / computed VS. watch

--- src/views/ComputedOption.vue
+++ src/views/ComputedOption.vue
@@ -7,12 +7,19 @@
 
 <template>
   <div>
-    <h3>computed 속성</h3>
+    <h3>--- computed 속성 : 연산에 의한 결과 반영 ---</h3>
     <p>FirstName : <input type="text" v-model="fname" /></p>
     <p>LastName : <input type="text" v-model="lname" /></p>
     <p>FullName : {{ fullName }}</p>
   </div>
   <div>
+    <h3>--- watch 속성 : 속성의 변경이전, 변경이후 값을 처리 ---</h3>
+    <strong>{{ changeName }}</strong>
+    <!-- v-bind, :, @ 모두 같은 의미 -->
+    <button @click="changingName">이름변경</button>
+  </div>
+  <div>
+    <h3>--- 장바구니 ---</h3>
     <p v-bind:key="i" v-for="(fruit, i) in cartList">
       <strong>{{ fruit.pname }}</strong> - 가격:
       {{ fruit.price.toLocaleString() }} 원 - 수량 :
@@ -40,6 +47,7 @@ export default {
         { id: 2, pname: "포도", price: 2000, qty: 3 },
         { id: 3, pname: "수박", price: 6000, qty: 1 },
       ],
+      changeName: "Hong",
     };
   },
   computed: {
@@ -56,6 +64,17 @@ export default {
       return this.cartList.reduce((acc, item) => {
         return acc + item.price * item.qty;
       }, 0);
+    },
+  },
+  watch: {
+    //watch는 watch에 정의된 데이터 값 하나만을 감시하기 위한 용도로 실제 데이터가 변경되기전까진 실행x
+    changeName(next, prev) {
+      console.log(`변경전: ${prev}, 변경후: ${next}`);
+    },
+  },
+  methods: {
+    changingName() {
+      this.changeName = "Hwang";
     },
   },
 };
