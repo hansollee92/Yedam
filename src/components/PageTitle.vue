4250d8dd70e03df239714c4e44c4c56db9ee2563
HANSOL LEE <hansol2886@gmail.com>
2025-09-01 09:59:38 +0900

    ✨부모->자식 다양한 데이터 타입으로 전달하는 방법(동적방법)

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="child-container">
+    {{ msg }}
+
+    <h3>{{ title }}</h3>
+
+    <p>Likes: {{ likes }}</p>
+    <p>isOk: {{ isOk ? "Yes" : "No" }}</p>
+
+    <h3>MemberList</h3>
+    <ul>
+      <li :key="i" v-for="(family, i) in memberList">
+        {{ family.name }} - {{ family.age }} year old.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Child Component",
+    };
+  },
+  props: {
+    title: { type: String, default: "Default Title" },
+    likes: { type: Number, default: 0 },
+    isOk: { type: Boolean, default: false },
+    memberList: { type: Array, default: () => [] },
+    // 객체(Object), 배열(Array)의 경우 초기값은 함수(function)으로 정의해줘야한다.
+    // () => [] 라는 의미 function(){return [];}와 같은 의미로 빈배열이라는 의미로 화살표함수로 표시한 것
+  },
+};
+</script>
+
+<style scoped>
+.child-container {
+  border: 1px solid #eee;
+  background-color: #f1d8e5;
+  padding: 10px;
+}
+</style>
---
+++ src/views/NestedComponent.vue
@@ -0,0 +1,45 @@
+<!-- views/NestedComponent.vue -->
+<!-- 부모컴포넌트 -> 자식컴포넌트로 데이터 전달하기 : props  -->
+
+<template>
+  <div class="parent-container">
+    {{ msg }}
+    <PageTitle
+      v-bind:title="title"
+      v-bind:likes="likeCnt"
+      v-bind:isOk="okVal"
+      :memberList="members"
+    />
+    <!-- 상위 컴포넌트에서 위와 같이 다양한 데이터타입으로 데이터를 전달해서 페이지를 구성할 수 있다 -->
+  </div>
+</template>
+
+<script>
+import PageTitle from "../components/PageTitle.vue";
+export default {
+  components: {
+    PageTitle,
+  },
+  data() {
+    return {
+      msg: "Parent Component",
+      title: "Dynamic Data from Parent",
+      likeCnt: 3,
+      okVal: true,
+      members: [
+        { name: "father", age: 40 },
+        { name: "mother", age: 35 },
+        { name: "son", age: 6 },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.parent-container {
+  border: 1px solid #eee;
+  background-color: #bbffe8;
+  padding: 10px;
+}
+</style>
